3c5f1045a35d1222eb98aad59218060ef558d6a3
Maksim Nikolaev <mksniklaev@gmail.com>
2024-02-10 15:06:58 +0500

    feat: домашняя страница

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../widgets/header";
+import styles from './home.module.css';
+
+
+export const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Header/>      
+    </div>
+  )
+}
