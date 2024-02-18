ef6f8b86303d6c5ce607a8e5097d97bd261f0843
Maksim Nikolaev <mksniklaev@gmail.com>
2024-02-19 01:09:20 +0500

    feat: умная обрезка

--- src/entities/posts/ui/post.tsx
+++ src/entities/posts/ui/post.tsx
@@ -1,12 +1,64 @@
 import styles from './posts.module.css';
 import { Raiting } from '../../../shared/ui/raiting/raiting';
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { TPost } from '../model';
 import { useNavigate } from 'react-router-dom';
 
 export const Post: FC<{ post: TPost }> = ({ post }) => {
   const { body, reactions, tags, title } = post;
   const navigate = useNavigate();
+
+  const [clippedText, setClippedText] = useState(body);
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  //Умная обрезка
+  useEffect(() => {
+    const clipText = () => {
+      const card = cardRef.current;
+      if (!card) return;
+
+      /* Получаем текущую высоту строки текста и вычисляем максимальную высоту карточки,
+      чтобы текст всегда оставался на трёх строках */
+      const lineHeight = parseInt(window.getComputedStyle(card).lineHeight);
+      const maxLines = 3;
+      const maxHeight = maxLines * lineHeight;
+
+      /* Разбиваем текст на слова, чтобы обрезать его по словам. 
+      Инициализируем переменные для обрезанного текста и текущей высоты */
+      const words = body.split(' ');
+      let clipped = '';
+      let currentHeight = 0;
+
+      /* Перебираем каждое слово в тексте. */
+      for (let word of words) {
+        /* Добавляем текущее слово к обрезанному тексту и обновляем текст в карточке,
+        чтобы получить текущую высоту карточки после добавления нового слова */
+        const newText = clipped + word + ' ';
+        card.innerText = newText + '...';
+        currentHeight = card.scrollHeight;
+        /* Если текущая высота карточки превышает максимальную высоту 
+        (т.е. текст уже занимает больше трёх строк), мы должны обрезать текст */
+        if (currentHeight > maxHeight) {
+          while (card.scrollHeight > maxHeight && clipped) {
+            clipped = clipped.slice(0, -1);
+            card.innerText = clipped + '...';
+          }
+          break;
+        }
+        
+        clipped = newText;
+      }
+      
+      setClippedText(clipped.trim() + '...');
+    };
+
+    clipText();
+    window.addEventListener('resize', clipText);
+
+    return () => {
+      window.removeEventListener('resize', clipText);
+    };
+  }, [body]);
 
   return (
     <article
@@ -18,7 +70,7 @@ export const Post: FC<{ post: TPost }> = ({ post }) => {
         <p className={styles.post_hash}>{tags}</p>
         <Raiting raiting={reactions} isRevert={true} />
       </div>
-      <p className={styles.post_text}>{body}</p>
+      <div ref={cardRef} className={styles.post_text}>{clippedText}</div>
     </article>
   );
 };
