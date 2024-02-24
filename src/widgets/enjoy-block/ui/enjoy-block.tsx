623338f1ac507e3f89f583b5fafed5407080d6ec
Maksim Nikolaev <mksniklaev@gmail.com>
2024-02-25 00:35:12 +0500

    ref: указал альтернативные форматы фотографий, если не поддерживается webp

--- src/widgets/enjoy-block/ui/enjoy-block.tsx
+++ src/widgets/enjoy-block/ui/enjoy-block.tsx
@@ -2,33 +2,42 @@ import { Button } from '../../../shared/ui/button/button';
 import { Notification } from '../../../shared/ui/notification/notification';
 import { SpanColor } from '../../../shared/ui/span-color/span-color';
 import styles from './enjoy-block.module.css';
-import food from '../../../shared/assets/icons/food.svg'
-import im3 from '../../../shared/assets/images/Graph.webp'
+import food from '../../../shared/assets/icons/food.svg';
+import graphWebp from '../../../shared/assets/images/Graph.webp';
+import graphPng from '../../../shared/assets/images/graph.png';
 import { notification } from '../model';
 
 export const EnjoyBlock = () => {
   return (
     <section className={styles.container}>
       <div className={styles.enjoy_food}>
-      <p className={styles.count_user}>OVER 1000 USERS</p>
-      <h1 className={styles.title}>
-        Enjoy Foods All Over The <SpanColor>World</SpanColor>
-      </h1>
-      <p className={styles.paragraph}>
-        EatLy help you set saving goals, earn cash back offers, Go to disclaimer
-        for more details and get paychecks up to two days early. Get a{' '}
-        <SpanColor>$20 bonus.</SpanColor>
-      </p>
-      <div className={styles.btn_group}>
-        <Button variant='primary'>Get Started</Button>
-        <Button variant='secondary'>Go Pro</Button>
+        <p className={styles.count_user}>OVER 1000 USERS</p>
+        <h1 className={styles.title}>
+          Enjoy Foods All Over The <SpanColor>World</SpanColor>
+        </h1>
+        <p className={styles.paragraph}>
+          EatLy help you set saving goals, earn cash back offers, Go to
+          disclaimer for more details and get paychecks up to two days early.
+          Get a <SpanColor>$20 bonus.</SpanColor>
+        </p>
+        <div className={styles.btn_group}>
+          <Button variant='primary'>Get Started</Button>
+          <Button variant='secondary'>Go Pro</Button>
+        </div>
       </div>
-    </div>
-    <div className={styles.enjoy_photo}>
-      <img className={styles.img} src={food} alt='plate of food' role='presentation'/>    
-      <Notification extraClass={styles.notification} card={notification}/>
-      <img className={styles.graph} src={im3} alt='line graph'/>
-    </div>
+      <div className={styles.enjoy_photo}>
+        <img
+          className={styles.img}
+          src={food}
+          alt='plate of food'
+          role='presentation'
+        />
+        <Notification extraClass={styles.notification} card={notification} />
+        <picture>
+          <source type='image/webp' srcSet={graphWebp} />
+          <img className={styles.graph} src={graphPng} alt='line graph' />
+        </picture>
+      </div>
     </section>
-  )
-}
+  );
+};
