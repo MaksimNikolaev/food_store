b1286c50a9a9de238801865a2cc2a364901c5bae
Maksim Nikolaev <mksniklaev@gmail.com>
2024-02-24 20:33:11 +0500

    feat: Если данные загружаются, скелеты карточек с лоадером

--- src/entities/posts/ui/posts.tsx
+++ src/entities/posts/ui/posts.tsx
@@ -2,23 +2,31 @@ import styles from './posts.module.css';
 import { Post } from './post';
 import { useSelector } from '../../../app/store';
 import { useNavigate } from 'react-router-dom';
+import Loader from '../../../shared/ui/loaders/loader';
 
 export const Posts = () => {
-  const { posts } = useSelector(state => state.posts);
+  const { posts, postsLoading } = useSelector(state => state.posts);
   const navigate = useNavigate();
-
-  if (!posts) {
-    return null;
-  }
 
   return (
     <div className={styles.posts_container}>
-      {posts?.posts.length ? (
-        posts?.posts.map(post =>
-          <article key={post.id} onClick={() => navigate(`/blog/post/${post.id}`)} className={styles.post}>
+      {postsLoading ? (
+        // Если данные загружаются, показать скелеты карточек с лоадером
+        Array.from({ length: 12 }).map((_, index) => (
+          <div className={styles.post_skelet}>
+            <Loader key={index} />
+          </div>
+        ))
+      ) : posts?.posts.length ? (
+        posts?.posts.map(post => (
+          <article
+            key={post.id}
+            onClick={() => navigate(`/blog/post/${post.id}`)}
+            className={styles.post}
+          >
             <Post post={post} />
-          </article>        
-        )
+          </article>
+        ))
       ) : (
         <p className={styles.no_posts}>Posts are not available</p>
       )}
