c30335ac91f343cc6be07f307501839e54b7cbc3
Maksim Nikolaev <mksniklaev@gmail.com>
2024-03-28 18:11:09 +0500

    feat: page balance

--- src/app/routing.tsx
+++ src/app/routing.tsx
@@ -3,6 +3,7 @@ import { Home } from '../pages/home/home';
 import { Blog } from '../pages/blog/blog';
 import { Post } from '../pages/post/post';
 import { NotFound } from '../pages/not-found/not-found';
+import { Tg } from '../pages/tg/tg';
 
 export const Routing = () => {
   return (
@@ -10,6 +11,7 @@ export const Routing = () => {
       <Route element={<Home />} path='/' />
       <Route element={<Blog />} path='/blog' />
       <Route element={<Post />} path='/blog/post/:id' />
+      <Route element={<Tg />} path='/tg/balance' />
       <Route element={<NotFound />} path='*' />
     </Routes>
   );
---
+++ src/pages/tg/tg.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Tg = () => {
+  return (
+    <div>tg</div>
+  )
+}
