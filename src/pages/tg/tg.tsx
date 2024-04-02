56d19078066088bd153af964cec038b8087b17e0
Maksim Nikolaev <mksniklaev@gmail.com>
2024-04-02 18:35:39 +0500

    feat: тест тг без хедера и футера

--- src/pages/tg/tg.tsx
+++ src/pages/tg/tg.tsx
@@ -1,16 +1,22 @@
 import { useEffect } from 'react';
 import style from './tg.module.css';
+import { Header } from '../../widgets/header';
+import { menuNavigationList } from '../../shared/utils/constants/constants';
+import { Footer } from '../../widgets/footer';
 
 export const Tg = () => {
   const tg = window.Telegram.WebApp;
   const token = localStorage.getItem('accessToken');
+  const isTelegramWebApp = window.navigator.userAgent.includes('TelegramBot');
 
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const handleAuthorization = () => {    
-    tg.openTelegramLink('https://t.me/BearsWb_bot?start=e356458e-11eb-4f79-baf7-c3da7ace3ae0');
+  const handleAuthorization = () => {
+    tg.openTelegramLink(
+      'https://t.me/BearsWb_bot?start=e356458e-11eb-4f79-baf7-c3da7ace3ae0'
+    );
   };
 
   useEffect(() => {
@@ -21,12 +27,18 @@ export const Tg = () => {
   }, []);
 
   return (
-    <div className={style.container}>
-      {token ? (
-        <h2 className={style.balance}>Ваш баланс: 12 Р</h2>
-      ) : (
-        <button onClick={handleAuthorization} className={style.button}>Пройти авторизацию</button>
-      )}
-    </div>
+    <>
+      {!isTelegramWebApp && <Header menuNavigationList={menuNavigationList} />}
+      <div className={style.container}>
+        {token ? (
+          <h2 className={style.balance}>Ваш баланс: 12 Р</h2>
+        ) : (
+          <button onClick={handleAuthorization} className={style.button}>
+            Пройти авторизацию
+          </button>
+        )}
+      </div>
+      {!isTelegramWebApp && <Footer />}
+    </>
   );
 };
